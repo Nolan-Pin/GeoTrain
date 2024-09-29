cf6fe439b169b0adb19e8aead16027757b5d8067
Nolan Pin <nolan.pin@outlook.com>
2024-09-29 21:52:17 +0200

    Keep track of score when choosing answer

--- country_shape.gd
+++ country_shape.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 const nb_of_guess = 4
 var solution: CountryAttributes
 var countries: AvailableCountry
+var round: int
+var found: int
+var not_found: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	countries = load("res://data/available_country.tres")
@@ -16,6 +19,10 @@ func _process(delta: float) -> void:
 
 func start_round(country_index: int) -> void:
 	solution = countries.available[country_index]
+	round = 0
+	found = 0
+	not_found = 0
+	update_score()
 	
 	$Country.set_texture(solution.image)
 	
@@ -29,3 +36,22 @@ func start_round(country_index: int) -> void:
 	$Choice2.text = choices[1].capitalize()
 	$Choice3.text = choices[2].capitalize()
 	$Choice4.text = choices[3].capitalize()
+
+
+func end_round(success: bool) -> void:
+	round += 1
+	if (success):
+		found += 1
+	else:
+		not_found += 1
+	update_score()
+
+
+func choice_made(selected: int) -> void:
+	var button: Button = find_child("Choice" + str(selected))
+	end_round(button.text.to_lower() == solution.en_name)
+
+func update_score() -> void:
+	$Scoring/Progress.text = str(round) + "/" + str(nb_of_guess)
+	$Scoring/Found.text = str(found)
+	$Scoring/NotFound.text = str(not_found)
